8900740bb317429d1b76f655eb4ccf53efb1e7ad
tsantiagoro85 <141088128+tsantiagoro85@users.noreply.github.com>
2024-08-04 13:30:13 -0500

    updated code and results

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,58 @@
-# Modules
+# Import modules
 import os
 import csv
 
-# Prompt user for title lookup
+budget_data_csv_path = os.path.join("Resources", "budget_data.csv") # Define path to dataset location
 
-# Set path for file
-csvpath = os.path.join("..", "Resources", "comic_books.csv")
+months = [] # Create a list for months
+profit = [] # Create a list for profit
 
-# Set variable to check if we found the video
+count_months = len(profit) # Define the total number of months
+total = sum(profit) # Define the total profits and losses
 
-# Open the CSV using the UTF-8 encoding
-with open(csvpath, encoding='UTF-8') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(budget_data_csv_path) as csvfile: # Open dataset as csv file
+    csv_reader = csv.reader(csvfile, delimiter = ",") # Read dataset as csv file
+    csv_header = next(csvfile) # Read header
 
-    # Loop through looking for the video
+    for row in csv_reader:  # Loop through each row
+        count_months = count_months + 1 # Count months
+        current_month = int(row[1]) # Specify the current month of profits and losses
+        total = total + current_month # Specify the total profits and losses amount
+        
+        # Set and if/else statement inside the for loop to determine the profit loss for previous and current months, and profit loss change
+        if (count_months == 1):
+            previous_month = current_month  # Make the value of previous month to be equal to current month
+        
+        else:
+            months.append(row[0]) # Append each month to list months[]
+            profit_change = current_month - previous_month # Compute change in profit loss
+            profit.append(profit_change) # Append each profit_change to the profit[]
+            previous_month = current_month # Make the current_month to be previous_month
+           
+    average_profit = round(sum(profit)/(count_months - 1), 2) # Determine average of profits and losses
 
-            # Set variable to confirm we have found the video
+    greatest_increase = max(profit) # Determine greatest increase in profits and losses
+    greatest_decrease = min(profit) # Determine greatest in profits and losses
 
-    # If the book is never found, alert the user
+    greatest_increase_month = months[profit.index(greatest_increase)] # Determine month of greatest increase
+    greatest_decrease_month= months[profit.index(greatest_decrease)] # Determine month of greatest decrease
+
+# Print the analysis results to the terminal
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {count_months}")
+print(f"Total: ${total}")
+print(f"Average Change: ${average_profit}")
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Losses: {greatest_decrease_month} (${greatest_decrease})")
+
+# Write a text file with the results printed in the terminal
+output = os.path.join("analysis", "budget_data_results.txt")
+with open(output, "w") as outfile:
+    outfile.write("Financial Analysis\n")
+    outfile.write("----------------------------\n")
+    outfile.write(f"Total Months: {count_months}\n")
+    outfile.write(f"Total: ${total}\n")
+    outfile.write(f"Average Change: ${average_profit}\n")
+    outfile.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})\n")
+    outfile.write(f"Greatest Decrease in Losses: {greatest_decrease_month} (${greatest_decrease})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,20 +1,66 @@
-# Modules
+# Import modules
 import os
 import csv
 
-# Prompt user for title lookup
+election_data_csv_path = os.path.join("Resources", "election_data.csv") # Define path to dataset location
 
-# Set path for file
-csvpath = os.path.join("..", "Resources", "comic_books.csv")
+vote_count = [] # Create a list to store vote counts
+candidates = [] # Create a list to store candidates
 
-# Set variable to check if we found the video
+votes = len(candidates) # Define votes
 
-# Open the CSV using the UTF-8 encoding
-with open(csvpath, encoding='UTF-8') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(election_data_csv_path) as csvfile: # Open dataset as csv file
+    csv_reader = csv.reader(csvfile, delimiter = ",") # Read dataset as csv file
+    next(csv_reader) # Read header
 
-    # Loop through looking for the video
+    for row in csv_reader: # Loop through each row
+        votes = votes + 1 # Count votes
+        candidate = row[2] # Specify candidates
 
-            # Set variable to confirm we have found the video
+        # Set if/else statement inside the for loop to determine the candidate index and the vote counts
+        if candidate in candidates:
+           candidate_index = candidates.index(candidate) # Specify the candidate index
+           vote_count[candidate_index] = vote_count[candidate_index] + 1 # Determine vote count
+           
+        else:
+           candidates.append(candidate) # Append each candidate to candidates list
+           vote_count.append(1) # Append each vote count
 
-    # If the book is never found, alert the user
+percentages = [] # Create a list to store percentages
+majority_votes = vote_count[0] # Define majority of votes
+majority_votes_index = 0 # Define the index for majority of votes
+
+for count in range(len(candidates)):
+    percentage = vote_count[count]/votes * 100
+    percentages.append(percentage)
+    if vote_count[count] > majority_votes:
+        print(majority_votes)
+        majority_votes_index = count
+        
+winner = candidates[majority_votes_index] # Define election winner
+percentages = [round (i,2) for i in percentages] # Determine percentages
+
+# Print results to the terminal
+print()
+print("Election Results")
+print("--------------------------------")
+print(f"Total Votes: {votes}")
+print("--------------------------------")
+for count in range(len(candidates)):
+    print(f"{candidates[count]}: {percentages[count]}% ({vote_count[count]})")
+print("--------------------------------")
+print(f"Winner:  {winner}")
+print("--------------------------------")
+ 
+# Write results printed in the terminal
+output = os.path.join("analysis", "election_data_results.txt") # Define output path
+with open(output, "w") as outfile:
+    outfile.write("Election Results\n")
+    outfile.write("----------------------------\n")
+    outfile.write(f"Total Votes: {votes}\n")
+    outfile.write("----------------------------\n")
+    for count in range(len(set(candidates))):
+        outfile.write(f"{candidates[count]}: {percentages[count]}% ({vote_count[count]})\n")
+    outfile.write("----------------------------\n")
+    outfile.write(f"Winner:  {winner}\n")
+    outfile.write("----------------------------\n")
