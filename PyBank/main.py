2913586959d124ea8acc741883879923de68a1da
tsantiagoro85 <141088128+tsantiagoro85@users.noreply.github.com>
2024-08-05 13:27:10 -0500

    updated main codes

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,40 +2,46 @@
 import os
 import csv
 
-budget_data_csv_path = os.path.join("Resources", "budget_data.csv") # Define path to dataset location
+# Define path to dataset location
+budget_data_csv_path = os.path.join("Resources", "budget_data.csv")
 
 months = [] # Create a list for months
 profit = [] # Create a list for profit
 
-count_months = len(profit) # Define the total number of months
+count_months = len(months) # Define the total number of months
 total = sum(profit) # Define the total profits and losses
 
-with open(budget_data_csv_path) as csvfile: # Open dataset as csv file
+# Open dataset
+with open(budget_data_csv_path) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ",") # Read dataset as csv file
     csv_header = next(csvfile) # Read header
 
-    for row in csv_reader:  # Loop through each row
+    # Loop through each row
+    for row in csv_reader:
         count_months = count_months + 1 # Count months
-        current_month = int(row[1]) # Specify the current month of profits and losses
-        total = total + current_month # Specify the total profits and losses amount
+        current_month = int(row[1]) # Define current month
+        total = total + current_month # Determine the total profits and losses amount
         
-        # Set and if/else statement inside the for loop to determine the profit loss for previous and current months, and profit loss change
+        # Set an if/else statement inside the for loop to determine the profits for previous and current months, and profit loss change
         if (count_months == 1):
             previous_month = current_month  # Make the value of previous month to be equal to current month
         
         else:
             months.append(row[0]) # Append each month to list months[]
-            profit_change = current_month - previous_month # Compute change in profit loss
-            profit.append(profit_change) # Append each profit_change to the profit[]
+            profit_change = current_month - previous_month # Determine the profit change
+            profit.append(profit_change) # Append each profit_change to list profit[]
             previous_month = current_month # Make the current_month to be previous_month
            
-    average_profit = round(sum(profit)/(count_months - 1), 2) # Determine average of profits and losses
+    # Determine average profit
+    average_profit = round(sum(profit)/(count_months - 1), 2)
 
-    greatest_increase = max(profit) # Determine greatest increase in profits and losses
-    greatest_decrease = min(profit) # Determine greatest in profits and losses
+    # Determine greatest increase and greatest decrease in profit
+    greatest_increase = max(profit)
+    greatest_decrease = min(profit)
 
-    greatest_increase_month = months[profit.index(greatest_increase)] # Determine month of greatest increase
-    greatest_decrease_month= months[profit.index(greatest_decrease)] # Determine month of greatest decrease
+    # Determine month of greatest increase and greatest decrease in profit
+    greatest_increase_month = months[profit.index(greatest_increase)]
+    greatest_decrease_month= months[profit.index(greatest_decrease)]
 
 # Print the analysis results to the terminal
 print("Financial Analysis")
@@ -46,7 +52,7 @@ print(f"Average Change: ${average_profit}")
 print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
 print(f"Greatest Decrease in Losses: {greatest_decrease_month} (${greatest_decrease})")
 
-# Write a text file with the results printed in the terminal
+# Write file with the results printed to the terminal
 output = os.path.join("analysis", "budget_data_results.txt")
 with open(output, "w") as outfile:
     outfile.write("Financial Analysis\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,43 +2,56 @@
 import os
 import csv
 
-election_data_csv_path = os.path.join("Resources", "election_data.csv") # Define path to dataset location
+# Define path to dataset location
+election_data_csv_path = os.path.join("Resources", "election_data.csv")
 
 vote_count = [] # Create a list to store vote counts
 candidates = [] # Create a list to store candidates
 
-votes = len(candidates) # Define votes
+votes = len(vote_count) # Define votes
 
-with open(election_data_csv_path) as csvfile: # Open dataset as csv file
+# Open dataset
+with open(election_data_csv_path) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter = ",") # Read dataset as csv file
     next(csv_reader) # Read header
 
-    for row in csv_reader: # Loop through each row
+    # Loop through each row
+    for row in csv_reader:
         votes = votes + 1 # Count votes
         candidate = row[2] # Specify candidates
 
-        # Set if/else statement inside the for loop to determine the candidate index and the vote counts
+        # Set if/else statement inside the for loop to determine the candidate index and then vote counts using the candidate index
         if candidate in candidates:
            candidate_index = candidates.index(candidate) # Specify the candidate index
            vote_count[candidate_index] = vote_count[candidate_index] + 1 # Determine vote count
            
         else:
-           candidates.append(candidate) # Append each candidate to candidates list
-           vote_count.append(1) # Append each vote count
+           candidates.append(candidate) # Append each candidate to list candidates[]
+           vote_count.append(1) # Append each vote count to list vote_count[]
 
-percentages = [] # Create a list to store percentages
-majority_votes = vote_count[0] # Define majority of votes
-majority_votes_index = 0 # Define the index for majority of votes
+# Create a list to store percentages
+percentages = []
 
+# Define majority of votes
+majority_votes = vote_count[0]
+
+# Define the index for majority of votes
+majority_votes_index = 0
+
+# Loop through each row
 for count in range(len(candidates)):
-    percentage = vote_count[count]/votes * 100
-    percentages.append(percentage)
-    if vote_count[count] > majority_votes:
-        print(majority_votes)
-        majority_votes_index = count
+    percentage = vote_count[count]/votes * 100 # Calculate percentage
+    percentages.append(percentage) # Append percentage to list percentages[]
+    
+    # Set if/else statement inside the for loop to specify
+    if vote_count[count] > majority_votes: # Specify vote counts relative to most votes
+        majority_votes_index = count # Specify majority vote index
         
-winner = candidates[majority_votes_index] # Define election winner
-percentages = [round (i,2) for i in percentages] # Determine percentages
+# Determine who is the election winner
+winner = candidates[majority_votes_index]
+
+# Determine percentages for each candidate
+percentages = [round (i,2) for i in percentages]
 
 # Print results to the terminal
 print()
